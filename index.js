ff2fe3d8b988f14abed8cf08e0a8f64c7543dacd
Khandker <khandker.islam46@gmail.com>
2020-06-24 20:41:45 -0400

    What is going on

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const cors = require('cors')
 const baseUrl = '/api/persons'
 const app = express()
 
+const path = require('path')// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'build')))// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'))
+})
+
 app.use(cors())
 app.use(express.static('build'))
 
